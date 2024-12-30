a7f3e0f6564fce9e75c75ddbaf00af53d076dfc7
christian <kadiuchristian8@gmail.com>
2024-12-30 10:25:53 +0100

    improved code of a01a.e1.SubsequenceCombinerFactoryImpl

--- src/oop2022-esami/a01a/e1/SubsequenceCombinerFactoryImpl.java
+++ src/oop2022-esami/a01a/e1/SubsequenceCombinerFactoryImpl.java
@@ -3,119 +3,173 @@ package a01a.e1;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
-public class SubsequenceCombinerFactoryImpl implements SubsequenceCombinerFactory{
+public class SubsequenceCombinerFactoryImpl implements SubsequenceCombinerFactory {
 
-    @Override
-    public SubsequenceCombiner<Integer, Integer> tripletsToSum() {
-        return new SubsequenceCombiner<Integer,Integer>() {
-                @Override
-                public List<Integer> combine(List<Integer> list) {
-                    int count = 1;
-                    int somma = 0;
-                    List<Integer> lista = new ArrayList<>();
-                    for (Integer integer : list) {
-                        somma = somma + integer;
-                        if(count % 3 == 0){
-                            lista.add(somma);
-                            somma = 0;
-                        }
-                        count++;
-                    }
-                    if (somma != 0){
-                        lista.add(somma);
-                    }
-                    return lista;
-                }
-        };
-    }
-
-    @Override
-    public <X> SubsequenceCombiner<X, List<X>> tripletsToList() {
-        return new SubsequenceCombiner<X,List<X>>() {
-
-            @Override
-            public List<List<X>> combine(List<X> list) {
-                List<List<X>> mat = new ArrayList<>();
-                List<X> raw = new ArrayList<>();
-                for (int i = 0; i < list.size(); i++ ) {
-                    raw.add(list.get(i));
-                    if (raw.size() == 3){
-                        mat.add(raw);
-                        raw = new ArrayList<>();
-                    }
-                }
-                if (!raw.isEmpty()){
-                    mat.add(raw);
-                }
-                return mat;
-            }
-            
-        };
-    }
-
-    @Override
-    public SubsequenceCombiner<Integer, Integer> countUntilZero() {
-        return new SubsequenceCombiner<Integer,Integer>() {
-
-            @Override
-            public List<Integer> combine(List<Integer> list) {
-                int count = 0;
-                List<Integer> lista = new ArrayList<>();
-                for (Integer integer : list) {
-                    count++;
-                    if(integer == 0){
-                        lista.add(count - 1);
-                        count = 0;
-                    }
-                }
-                if(count != 0){
-                    lista.add(count);
-                }
-                return lista;
-            }
-        };
-    }
-
-    @Override
-    public <X, Y> SubsequenceCombiner<X, Y> singleReplacer(Function<X, Y> function) {
-        return new SubsequenceCombiner<X,Y>() {
+    private <X, Y> SubsequenceCombiner<X, Y> generic(Predicate<List<X>> ready, Function<List<X>, Y> mapper) {
+        return new SubsequenceCombiner<X, Y>() {
 
             @Override
             public List<Y> combine(List<X> list) {
                 List<Y> lista = new ArrayList<>();
+                List<X> tempList = new ArrayList<>();
                 for (X x : list) {
-                    lista.add(function.apply(x));
+                    tempList.add(x);
+                    if (ready.test(tempList)) {
+                        lista.add(mapper.apply(tempList));
+                        tempList = new ArrayList<>();
+                    }
+                }
+                if (!tempList.isEmpty()) {
+                    lista.add(mapper.apply(tempList));
                 }
                 return lista;
             }
-            
         };
     }
 
     @Override
-    public SubsequenceCombiner<Integer, List<Integer>> cumulateToList(int threshold) {
-        return new SubsequenceCombiner<Integer,List<Integer>>() {
-
-            @Override
-            public List<List<Integer>> combine(List<Integer> list) {
-                List<List<Integer>> lista = new ArrayList<>();
-                List<Integer> raw = new ArrayList<>();
-                int somma = 0;
-                for (Integer integer : list) {
-                    somma = somma + integer;
-                    raw.add(integer);
-                    if (somma >= 100){
-                        lista.add(raw);
-                        raw = new ArrayList<>();
-                        somma = 0;
-                    }
-                }
-                lista.add(raw);
-                return lista;
-            }
-            
-        };
+    public SubsequenceCombiner<Integer, Integer> tripletsToSum() {
+        return generic(l -> l.size() == 3, l -> l.stream().mapToInt(i -> i).sum());
     }
-    
+
+    @Override
+    public <X> SubsequenceCombiner<X, List<X>> tripletsToList() {
+        return generic(l -> l.size() == 3, /* ArrayList::new */ l -> new ArrayList<>(l));
+    }
+
+    @Override
+    public SubsequenceCombiner<Integer, Integer> countUntilZero() {
+        return generic(l -> !l.isEmpty() && l.get(l.size() - 1) == 0,
+                l -> l.get(l.size() - 1) == 0 ? l.size() - 1 : l.size());
+    }
+
+    @Override
+    public <X, Y> SubsequenceCombiner<X, Y> singleReplacer(Function<X, Y> function) {
+        return generic(l -> l.size() == 1, l -> function.apply(l.get(0)));
+    }
+
+    @Override
+    public SubsequenceCombiner<Integer, List<Integer>> cumulateToList(int threshold) {
+        return generic(l -> l.stream().mapToInt(i -> i).sum() >= 100, ArrayList::new);
+    }
+
+    /*
+     * @Override
+     * public SubsequenceCombiner<Integer, Integer> tripletsToSum() {
+     * return new SubsequenceCombiner<Integer,Integer>() {
+     * 
+     * @Override
+     * public List<Integer> combine(List<Integer> list) {
+     * int count = 1;
+     * int somma = 0;
+     * List<Integer> lista = new ArrayList<>();
+     * for (Integer integer : list) {
+     * somma = somma + integer;
+     * if(count % 3 == 0){
+     * lista.add(somma);
+     * somma = 0;
+     * }
+     * count++;
+     * }
+     * if (somma != 0){
+     * lista.add(somma);
+     * }
+     * return lista;
+     * }
+     * };
+     * }
+     * 
+     * @Override
+     * public <X> SubsequenceCombiner<X, List<X>> tripletsToList() {
+     * return new SubsequenceCombiner<X,List<X>>() {
+     * 
+     * @Override
+     * public List<List<X>> combine(List<X> list) {
+     * List<List<X>> mat = new ArrayList<>();
+     * List<X> raw = new ArrayList<>();
+     * for (int i = 0; i < list.size(); i++ ) {
+     * raw.add(list.get(i));
+     * if (raw.size() == 3){
+     * mat.add(raw);
+     * raw = new ArrayList<>();
+     * }
+     * }
+     * if (!raw.isEmpty()){
+     * mat.add(raw);
+     * }
+     * return mat;
+     * }
+     * 
+     * };
+     * }
+     * 
+     * @Override
+     * public SubsequenceCombiner<Integer, Integer> countUntilZero() {
+     * return new SubsequenceCombiner<Integer,Integer>() {
+     * 
+     * @Override
+     * public List<Integer> combine(List<Integer> list) {
+     * int count = 0;
+     * List<Integer> lista = new ArrayList<>();
+     * for (Integer integer : list) {
+     * count++;
+     * if(integer == 0){
+     * lista.add(count - 1);
+     * count = 0;
+     * }
+     * }
+     * if(count != 0){
+     * lista.add(count);
+     * }
+     * return lista;
+     * }
+     * };
+     * }
+     * 
+     * @Override
+     * public <X, Y> SubsequenceCombiner<X, Y> singleReplacer(Function<X, Y>
+     * function) {
+     * return new SubsequenceCombiner<X,Y>() {
+     * 
+     * @Override
+     * public List<Y> combine(List<X> list) {
+     * List<Y> lista = new ArrayList<>();
+     * for (X x : list) {
+     * lista.add(function.apply(x));
+     * }
+     * return lista;
+     * }
+     * 
+     * };
+     * }
+     * 
+     * @Override
+     * public SubsequenceCombiner<Integer, List<Integer>> cumulateToList(int
+     * threshold) {
+     * return new SubsequenceCombiner<Integer,List<Integer>>() {
+     * 
+     * @Override
+     * public List<List<Integer>> combine(List<Integer> list) {
+     * List<List<Integer>> lista = new ArrayList<>();
+     * List<Integer> raw = new ArrayList<>();
+     * int somma = 0;
+     * for (Integer integer : list) {
+     * somma = somma + integer;
+     * raw.add(integer);
+     * if (somma >= 100){
+     * lista.add(raw);
+     * raw = new ArrayList<>();
+     * somma = 0;
+     * }
+     * }
+     * lista.add(raw);
+     * return lista;
+     * }
+     * 
+     * };
+     * }
+     */
+
 }
--- src/oop2022-esami/a01a/sol1/SubsequenceCombinerFactoryImpl.java
+++ src/oop2022-esami/a01a/sol1/SubsequenceCombinerFactoryImpl.java
@@ -7,42 +7,42 @@ import java.util.function.Predicate;
 
 public class SubsequenceCombinerFactoryImpl implements SubsequenceCombinerFactory {
 
-    private <X,Y> SubsequenceCombiner<X,Y> generic(Predicate<List<X>> ready, Function<List<X>,Y> mapper){
-        return new SubsequenceCombiner<X,Y>() {
+    private <X, Y> SubsequenceCombiner<X, Y> generic(Predicate<List<X>> ready, Function<List<X>, Y> mapper) {
+        return new SubsequenceCombiner<X, Y>() {
 
             @Override
             public List<Y> combine(List<X> list) {
                 final List<Y> outList = new ArrayList<>();
                 final List<X> tempList = new ArrayList<>();
-                for (final var x: list){
+                for (final var x : list) {
                     tempList.add(x);
-                    if (ready.test(tempList)){
+                    if (ready.test(tempList)) {
                         outList.add(mapper.apply(tempList));
                         tempList.clear();
                     }
                 }
-                if (!tempList.isEmpty()){
+                if (!tempList.isEmpty()) {
                     outList.add(mapper.apply(tempList));
                 }
                 return outList;
             }
         };
-    } 
+    }
 
     @Override
-    public SubsequenceCombiner<Integer,Integer> tripletsToSum() {
+    public SubsequenceCombiner<Integer, Integer> tripletsToSum() {
         return generic(l -> l.size() == 3, l -> l.stream().mapToInt(i -> i).sum());
     }
 
     @Override
-    public <X> SubsequenceCombiner<X,List<X>> tripletsToList() {
+    public <X> SubsequenceCombiner<X, List<X>> tripletsToList() {
         return generic(l -> l.size() == 3, ArrayList::new);
     }
-    
+
     @Override
     public SubsequenceCombiner<Integer, Integer> countUntilZero() {
-        return generic(l -> !l.isEmpty() && l.get(l.size()-1) == 0, 
-            l -> l.get(l.size()-1) == 0 ? l.size() - 1 : l.size());
+        return generic(l -> !l.isEmpty() && l.get(l.size() - 1) == 0,
+                l -> l.get(l.size() - 1) == 0 ? l.size() - 1 : l.size());
     }
 
     @Override

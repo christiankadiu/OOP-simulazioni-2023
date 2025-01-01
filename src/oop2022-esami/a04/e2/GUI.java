87ff423d57cfef41d5291c6876074e534ca8d866
christiankadiu <kadiuchristian8@gmail.com>
2025-01-02 00:47:48 +0100

    minor changes in exercise e2 in a04

--- src/oop2022-esami/a04/e2/GUI.java
+++ src/oop2022-esami/a04/e2/GUI.java
@@ -8,9 +8,11 @@ import java.awt.event.*;
 
 public class GUI extends JFrame {
     
-    private final List<JButton> cells = new ArrayList<>();
+    private final Map<JButton, Pair<Integer, Integer>> cells = new HashMap<>();
+    Logics logic;
     
     public GUI(int size) {
+        logic = new LogicsImpl(size);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(100*size, 100*size);
         
@@ -20,18 +22,37 @@ public class GUI extends JFrame {
         ActionListener al = new ActionListener(){
             public void actionPerformed(ActionEvent e){
         	    var button = (JButton)e.getSource();
-        	    var position = cells.indexOf(button);
+        	    var position = cells.get(button);
                 button.setText(""+position);
             }
         };
-                
+
+        Random r = new Random();
+        int uno;
+        int due;
+        do{
+            uno = r.nextInt(size*size) + 1;
+            due = r.nextInt(size*size) + 1;
+        }while(uno == due);
         for (int i=0; i<size; i++){
             for (int j=0; j<size; j++){
                 final JButton jb = new JButton(" ");
-                this.cells.add(jb);
+                this.cells.put(jb, new Pair<>(i, j));
                 jb.addActionListener(al);
                 panel.add(jb);
             }
+        }
+        System.out.println("uno vale: "+uno);
+        System.out.println("due vale: "+due);
+        int i = 0;
+        for (Map.Entry<JButton, Pair<Integer, Integer>> entry : cells.entrySet()) {
+            if (i == uno){
+                entry.getKey().setText("R");
+            }
+            if (i == due){
+                entry.getKey().setText("K");
+            }
+            i++;
         }
         this.setVisible(true);
     }    
---
+++ src/oop2022-esami/a04/e2/Logics.java
@@ -0,0 +1,5 @@
+package a04.e2;
+
+public interface Logics {
+
+}

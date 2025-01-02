ee00be6ee1ebd27d6fc0bb343b0565ba64aa5542
christian <kadiuchristian8@gmail.com>
2025-01-02 11:48:46 +0100

    solved exercise e2 in a04

--- src/oop2022-esami/a04/e2/GUI.java
+++ src/oop2022-esami/a04/e2/GUI.java
@@ -2,58 +2,77 @@ package a04.e2;
 
 import javax.swing.*;
 import java.util.*;
-import java.util.List;
 import java.awt.*;
 import java.awt.event.*;
 
 public class GUI extends JFrame {
-    
+
     private final Map<JButton, Pair<Integer, Integer>> cells = new HashMap<>();
     Logics logic;
-    
+    int size;
+
     public GUI(int size) {
+        this.size = size;
         logic = new LogicsImpl(size);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(100*size, 100*size);
-        
-        JPanel panel = new JPanel(new GridLayout(size,size));
+        this.setSize(100 * size, 100 * size);
+
+        JPanel panel = new JPanel(new GridLayout(size, size));
         this.getContentPane().add(panel);
-        
-        ActionListener al = new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-        	    var button = (JButton)e.getSource();
-        	    var position = cells.get(button);
-                button.setText(""+position);
+
+        ActionListener al = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                var button = (JButton) e.getSource();
+                var position = cells.get(button);
+                if (logic.hit(position)) {
+                    button.setText("K");
+                }
+                updateCells();
+                if (logic.toQuit()) {
+                    if (logic.win()) {
+                        System.out.println("sconfitta");
+                    } else {
+                        System.out.println("vittoria");
+                    }
+                    logic = new LogicsImpl(size);
+                }
             }
         };
 
-        Random r = new Random();
-        int uno;
-        int due;
-        do{
-            uno = r.nextInt(size*size) + 1;
-            due = r.nextInt(size*size) + 1;
-        }while(uno == due);
-        for (int i=0; i<size; i++){
-            for (int j=0; j<size; j++){
-                final JButton jb = new JButton(" ");
-                this.cells.put(jb, new Pair<>(i, j));
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                final JButton jb = new JButton();
+                Pair<Integer, Integer> pos = new Pair<Integer, Integer>(i, j);
+                switch (logic.isSomething(pos)) {
+                    case 1:
+                        jb.setText("K");
+                        break;
+                    case 2:
+                        jb.setText("R");
+                        break;
+                    case 0:
+                        jb.setText(" ");
+                }
+                this.cells.put(jb, pos);
                 jb.addActionListener(al);
                 panel.add(jb);
             }
         }
-        System.out.println("uno vale: "+uno);
-        System.out.println("due vale: "+due);
-        int i = 0;
-        for (Map.Entry<JButton, Pair<Integer, Integer>> entry : cells.entrySet()) {
-            if (i == uno){
-                entry.getKey().setText("R");
-            }
-            if (i == due){
-                entry.getKey().setText("K");
-            }
-            i++;
-        }
         this.setVisible(true);
-    }    
+    }
+
+    void updateCells() {
+        for (Map.Entry<JButton, Pair<Integer, Integer>> entry : cells.entrySet()) {
+            switch (logic.isSomething(entry.getValue())) {
+                case 1:
+                    entry.getKey().setText("K");
+                    break;
+                case 2:
+                    entry.getKey().setText("R");
+                    break;
+                case 0:
+                    entry.getKey().setText(" ");
+            }
+        }
+    }
 }

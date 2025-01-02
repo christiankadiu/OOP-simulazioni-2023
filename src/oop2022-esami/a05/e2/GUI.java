142186e2e76e8728232fe35c51cc7ab49f0dc17d
christian <kadiuchristian8@gmail.com>
2025-01-02 17:05:32 +0100

    solved exercise e2 in a05

--- src/oop2022-esami/a05/e2/GUI.java
+++ src/oop2022-esami/a05/e2/GUI.java
@@ -7,32 +7,77 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class GUI extends JFrame {
-    
-    private final List<JButton> cells = new ArrayList<>();
-    
+
+    private final Map<JButton, Pair<Integer, Integer>> cells = new HashMap<>();
+    Logics logic;
+
     public GUI(int size) {
+        logic = new LogicsImpl(size);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(100*size, 100*size);
-        
-        JPanel panel = new JPanel(new GridLayout(size,size));
+        this.setSize(100 * size, 100 * size);
+
+        JPanel panel = new JPanel(new GridLayout(size, size));
         this.getContentPane().add(panel);
-        
-        ActionListener al = new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-        	    var button = (JButton)e.getSource();
-        	    var position = cells.indexOf(button);
-                button.setText(""+position);
+
+        ActionListener al = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                var button = (JButton) e.getSource();
+                var position = cells.get(button);
+                logic.hit(position);
+                updateCells();
+                if (logic.toQuit()) {
+                    System.exit(0);
+                }
             }
         };
-                
-        for (int i=0; i<size; i++){
-            for (int j=0; j<size; j++){
-                final JButton jb = new JButton(" ");
-                this.cells.add(jb);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                final JButton jb = new JButton();
+                Pair<Integer, Integer> pos = new Pair<Integer, Integer>(i, j);
+                this.cells.put(jb, pos);
                 jb.addActionListener(al);
                 panel.add(jb);
+                if (i % 2 == 0) {
+                    if (j % 2 != 0) {
+                        jb.setEnabled(false);
+                    }
+                } else {
+                    if (j % 2 == 0) {
+                        jb.setEnabled(false);
+                    }
+                }
+
+                if (jb.isEnabled()) {
+                    switch (logic.isSomething(pos)) {
+                        case 0:
+                            jb.setText("");
+                            break;
+                        case 1:
+                            jb.setText("H");
+                            break;
+                        case 2:
+                            jb.setText("C");
+                            break;
+                    }
+                }
             }
         }
         this.setVisible(true);
-    }    
+    }
+
+    void updateCells() {
+        for (Map.Entry<JButton, Pair<Integer, Integer>> entry : cells.entrySet()) {
+            switch (logic.isSomething(entry.getValue())) {
+                case 0:
+                    entry.getKey().setText("");
+                    break;
+                case 1:
+                    entry.getKey().setText("H");
+                    break;
+                case 2:
+                    entry.getKey().setText("C");
+                    break;
+            }
+        }
+    }
 }
---
+++ src/oop2022-esami/a05/e2/Logics.java
@@ -0,0 +1,11 @@
+package a05.e2;
+
+public interface Logics {
+
+    int isSomething(Pair<Integer, Integer> pos);
+
+    void hit(Pair<Integer, Integer> position);
+
+    boolean toQuit();
+
+}

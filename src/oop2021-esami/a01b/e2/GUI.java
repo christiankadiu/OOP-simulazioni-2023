95981f4b8541ea78c6af1c6e36649e133fc7964c
christiankadiu <kadiuchristian8@gmail.com>
2025-01-06 15:11:32 +0100

    minor changes

--- src/oop2021-esami/a01b/e2/GUI.java
+++ src/oop2021-esami/a01b/e2/GUI.java
@@ -9,9 +9,11 @@ import java.awt.event.ActionListener;
 public class GUI extends JFrame {
     
     private static final long serialVersionUID = -6218820567019985015L;
-    private final List<JButton> cells = new ArrayList<>();
+    private final Map<JButton, Pair<Integer, Integer>> cells = new HashMap<>();
+    Logics logic;
     
     public GUI(int size) {
+        logic = new LogicsImpl(size);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(50*size, 50*size);
         
@@ -20,14 +22,14 @@ public class GUI extends JFrame {
         
         ActionListener al = e -> {
         	var button = (JButton)e.getSource();
-        	button.setText(""+cells.indexOf(button));
+        	button.setText(""+cells.get(button));
         	button.setEnabled(false); 
         };
                 
         for (int i=0; i<size; i++){
             for (int j=0; j<size; j++){
                 final JButton jb = new JButton(" ");
-                this.cells.add(jb);
+                this.cells.put(jb, new Pair<>(i, j));
                 jb.addActionListener(al);
                 panel.add(jb);
             }
---
+++ src/oop2021-esami/a01b/e2/LogicsImpl.java
@@ -0,0 +1,10 @@
+package a01b.e2;
+
+public class LogicsImpl implements Logics {
+
+    int size;
+
+    LogicsImpl(int size){
+        this.size = size;
+    }
+}

42fb9b192d30e8aca98302bf402a0e58b61b766b
christian <kadiuchristian8@gmail.com>
2025-01-07 10:13:02 +0100

    solved exercise e1 in a03b

---
+++ src/oop2023-esami/a03b/e1/InfiniteIteratorHelpersImpl.java
@@ -0,0 +1,77 @@
+package a03b.e1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.Iterator;
+import java.util.stream.Stream;
+
+public class InfiniteIteratorHelpersImpl implements InfiniteIteratorsHelpers {
+
+    @Override
+    public <X> InfiniteIterator<X> of(X x) {
+        return new InfiniteIterator<X>() {
+
+            @Override
+            public X nextElement() {
+                return x;
+            }
+
+        };
+    }
+
+    @Override
+    public <X> InfiniteIterator<X> cyclic(List<X> l) {
+        return new InfiniteIterator<X>() {
+            List<X> lista = new ArrayList<>(l);
+            int current = 0;
+
+            @Override
+            public X nextElement() {
+                if (current == lista.size()) {
+                    current = 0;
+                }
+                return lista.get(current++);
+            }
+
+        };
+    }
+
+    @Override
+    public InfiniteIterator<Integer> incrementing(int start, int increment) {
+        return new InfiniteIterator<Integer>() {
+            int current = 0;
+
+            @Override
+            public Integer nextElement() {
+                return start + (increment * this.current++);
+            }
+
+        };
+    }
+
+    @Override
+    public <X> InfiniteIterator<X> alternating(InfiniteIterator<X> i, InfiniteIterator<X> j) {
+        return new InfiniteIterator<X>() {
+            boolean current = true;
+
+            @Override
+            public X nextElement() {
+                if (current) {
+                    current = false;
+                    return i.nextElement();
+                }
+                current = true;
+                return j.nextElement();
+            }
+
+        };
+    }
+
+    @Override
+    public <X> InfiniteIterator<List<X>> window(InfiniteIterator<X> i, int n) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'window'");
+    }
+
+}

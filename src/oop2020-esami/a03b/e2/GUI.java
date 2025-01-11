32e1406432b6d5c2355e41c055b955eee0f99fc4
christian <kadiuchristian8@gmail.com>
2025-01-11 15:25:32 +0100

    implemented LogicsImpl.java e Logics.java in a03b.e2

--- src/oop2020-esami/a03b/e2/GUI.java
+++ src/oop2020-esami/a03b/e2/GUI.java
@@ -6,38 +6,46 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
-    
+
     private static final long serialVersionUID = -6218820567019985015L;
-    private final Map<JButton,Pair<Integer,Integer>> cells = new HashMap<>();
-    
+    private final Map<JButton, Pair<Integer, Integer>> cells = new HashMap<>();
+    Logics logic;
+
     public GUI(int size) {
+        logic = new LogicsImpl(size);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(100*size, 100*size);
-        
-        JPanel grid = new JPanel(new GridLayout(size,size));
-        this.getContentPane().add(BorderLayout.CENTER,grid);
-        
+        this.setSize(100 * size, 100 * size);
+
+        JPanel grid = new JPanel(new GridLayout(size, size));
+        this.getContentPane().add(BorderLayout.CENTER, grid);
+
         ActionListener el = e -> {
-        	var button = (JButton)e.getSource();
-        	var position = this.cells.get(button);
-        	button.setText(position.getX()+":"+position.getY());
-        	button.setEnabled(false);
-        	
+            var button = (JButton) e.getSource();
+            var position = this.cells.get(button);
+            logic.hit(position);
+            draw();
         };
-                
-        for (int i=0; i<size; i++){
-            for (int j=0; j<size; j++){
-                final JButton jb = new JButton(" ");
-                this.cells.put(jb, new Pair<>(j,i));
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                final JButton jb = new JButton();
+                this.cells.put(jb, new Pair<>(j, i));
                 grid.add(jb);
                 jb.addActionListener(el);
             }
         }
-        var random = new Random();
-    	var randomPosition = new Pair<>(random.nextInt(size),random.nextInt(size));
-    	cells.forEach( (button,position) -> { if (position.equals(randomPosition)) {button.setText("*");}});
-    	
+        draw();
         this.setVisible(true);
     }
-    
+
+    public void draw() {
+        for (Map.Entry<JButton, Pair<Integer, Integer>> entry : cells.entrySet()) {
+            entry.getKey()
+                    .setText(logic.get(entry.getValue()) == 1 ? "B" : logic.get(entry.getValue()) == 2 ? "*" : " ");
+            if (!logic.isEnabled(entry.getValue())) {
+                entry.getKey().setEnabled(false);
+            }
+        }
+    }
+
 }
---
+++ src/oop2020-esami/a03b/e2/LogicsImpl.java
@@ -0,0 +1,84 @@
+package a03b.e2;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+public class LogicsImpl implements Logics {
+
+    int size;
+    List<Pair<Integer, Integer>> nemici;
+    Pair<Integer, Integer> bishop;
+    Set<Pair<Integer, Integer>> enabled;
+
+    LogicsImpl(int size) {
+        Random r = new Random();
+        this.size = size;
+        bishop = new Pair<Integer, Integer>(r.nextInt(size), r.nextInt(size));
+        calculate(bishop);
+        nemici = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            Pair<Integer, Integer> p = new Pair<Integer, Integer>(r.nextInt(size), r.nextInt(size));
+            if (!nemici.contains(p) && !p.equals(bishop)) {
+                nemici.add(p);
+            }
+        }
+    }
+
+    @Override
+    public int get(Pair<Integer, Integer> value) {
+        if (value.equals(bishop)) {
+            return 1;
+        }
+        if (nemici.contains(value)) {
+            return 2;
+        }
+        return 0;
+    }
+
+    private void calculate(Pair<Integer, Integer> pos) {
+        enabled = new HashSet<>();
+        for (int i = pos.getX(), k = pos.getY(); i < this.size && k < this.size; i++, k++) {
+            Pair<Integer, Integer> p = new Pair<Integer, Integer>(i, k);
+            if (!enabled.contains(p) && !p.equals(pos)) {
+                enabled.add(p);
+            }
+        }
+        for (int i = pos.getX(), k = pos.getY(); i >= 0 && k >= 0; i--, k--) {
+            Pair<Integer, Integer> p = new Pair<Integer, Integer>(i, k);
+            if (!enabled.contains(p) && !p.equals(pos)) {
+                enabled.add(p);
+            }
+        }
+        for (int i = pos.getX(), k = pos.getY(); i >= 0 && k < this.size; i--, k++) {
+            Pair<Integer, Integer> p = new Pair<Integer, Integer>(i, k);
+            if (!enabled.contains(p) && !p.equals(pos)) {
+                enabled.add(p);
+            }
+        }
+        for (int i = pos.getX(), k = pos.getY(); i < size && k >= 0; i++, k--) {
+            Pair<Integer, Integer> p = new Pair<Integer, Integer>(i, k);
+            if (!enabled.contains(p) && !p.equals(pos)) {
+                enabled.add(p);
+            }
+        }
+    }
+
+    @Override
+    public boolean isEnabled(Pair<Integer, Integer> value) {
+        return enabled.contains(value);
+    }
+
+    @Override
+    public void hit(Pair<Integer, Integer> pos) {
+        if (enabled.contains(pos)) {
+            if (nemici.contains(pos)) {
+                nemici.remove(pos);
+            }
+            bishop = pos;
+            calculate(pos);
+        }
+    }
+}

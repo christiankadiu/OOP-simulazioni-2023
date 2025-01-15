8789a97e500abd820e53133204524e20adefb8e7
christian <kadiuchristian8@gmail.com>
2025-01-15 22:12:02 +0100

    solved exercise e2 in a01a

--- src/oop2021-esami/a01a/e2/GUI.java
+++ src/oop2021-esami/a01a/e2/GUI.java
@@ -1,38 +1,56 @@
 package a01a.e2;
 
 import javax.swing.*;
+
 import java.util.*;
 import java.util.List;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
-    
+
     private static final long serialVersionUID = -6218820567019985015L;
-    private final List<JButton> cells = new ArrayList<>();
-    
+    private final Map<JButton, Pair<Integer, Integer>> cells = new HashMap<>();
+    Logics logic;
+
     public GUI(int size) {
+        logic = new LogicsImpl(size);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(50*size, 50*size);
-        
-        JPanel panel = new JPanel(new GridLayout(size,size));
+        this.setSize(50 * size, 50 * size);
+
+        JPanel panel = new JPanel(new GridLayout(size, size));
         this.getContentPane().add(panel);
-        
+
         ActionListener al = e -> {
-        	var button = (JButton)e.getSource();
-        	button.setText(""+cells.indexOf(button));
-        	button.setEnabled(false); 
+            var button = (JButton) e.getSource();
+            var pos = cells.get(button);
+            if (logic.hit(pos)) {
+                button.setText("" + 1);
+            } else {
+                draw();
+            }
+            if (logic.toQuit()) {
+                System.exit(0);
+            }
         };
-                
-        for (int i=0; i<size; i++){
-            for (int j=0; j<size; j++){
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 final JButton jb = new JButton(" ");
-                this.cells.add(jb);
+                this.cells.put(jb, new Pair<Integer, Integer>(i, j));
                 jb.addActionListener(al);
                 panel.add(jb);
             }
         }
         this.setVisible(true);
     }
-    
+
+    private void draw() {
+        for (Map.Entry<JButton, Pair<Integer, Integer>> entry : cells.entrySet()) {
+            if (logic.get(entry.getValue())) {
+                entry.getKey().setText("*");
+            }
+        }
+    }
+
 }
---
+++ src/oop2021-esami/a01a/e2/Logics.java
@@ -0,0 +1,11 @@
+package a01a.e2;
+
+public interface Logics {
+
+    boolean hit(Pair<Integer, Integer> pos);
+
+    boolean get(Pair<Integer, Integer> value);
+
+    boolean toQuit();
+
+}

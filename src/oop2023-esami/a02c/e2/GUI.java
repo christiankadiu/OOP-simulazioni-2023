a710f9952b04835383754beb875ad74e334efc0d
christian <kadiuchristian8@gmail.com>
2025-01-17 22:55:35 +0100

    started implementing logic of a02c.e2

--- src/oop2023-esami/a02c/e2/GUI.java
+++ src/oop2023-esami/a02c/e2/GUI.java
@@ -2,37 +2,48 @@ package a02c.e2;
 
 import javax.swing.*;
 import java.util.*;
-import java.util.List;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
-    
+
     private static final long serialVersionUID = -6218820567019985015L;
-    private final List<JButton> cells = new ArrayList<>();
-    
+    private final Map<JButton, Position> cells = new HashMap<>();
+    Logics logic;
+
     public GUI(int size) {
+        logic = new LogicsImpl(size);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(100*size, 100*size);
-        
-        JPanel panel = new JPanel(new GridLayout(size,size));
+        this.setSize(100 * size, 100 * size);
+
+        JPanel panel = new JPanel(new GridLayout(size, size));
         this.getContentPane().add(panel);
-        
+
         ActionListener al = e -> {
-            var jb = (JButton)e.getSource();
-        	jb.setText(String.valueOf(cells.indexOf(jb)));
+            var jb = (JButton) e.getSource();
+            Position pos = cells.get(jb);
+            logic.hit(pos);
+            draw();
         };
-                
-        for (int i=0; i<size; i++){
-            for (int j=0; j<size; j++){
-            	var pos = new Pair<>(j,i);
-                final JButton jb = new JButton(pos.toString());
-                this.cells.add(jb);
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                var pos = new Position(j, i);
+                final JButton jb = new JButton();
+                this.cells.put(jb, pos);
                 jb.addActionListener(al);
                 panel.add(jb);
             }
         }
         this.setVisible(true);
     }
-    
+
+    private void draw() {
+        for (Map.Entry<JButton, Position> entry : cells.entrySet()) {
+            if (logic.isPresent(entry.getValue())) {
+                entry.getKey().setText("*");
+            }
+        }
+    }
+
 }
--- src/oop2023-esami/a02c/e2/Logics.java
+++ src/oop2023-esami/a02c/e2/Logics.java
@@ -2,4 +2,8 @@ package a02c.e2;
 
 public interface Logics {
 
+    void hit(Position pos);
+
+    boolean isPresent(Position value);
+
 }

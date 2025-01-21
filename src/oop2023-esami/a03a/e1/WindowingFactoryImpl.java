21b534280805337e5f5b8e5f656a5d5c373ece83
Christian Kadiu - christian.kadiu@studio.unibo.it <christian.kadiu@studio.unibo.it>
2025-01-21 16:00:28 +0100

    solved exercise e1 in a03a

--- src/oop2023-esami/a03a/e1/WindowingFactoryImpl.java
+++ src/oop2023-esami/a03a/e1/WindowingFactoryImpl.java
@@ -8,75 +8,69 @@ public class WindowingFactoryImpl implements WindowingFactory {
 
     @Override
     public <X> Windowing<X, X> trivial() {
-        return new Windowing<X, X>() {
+        return new Windowing<X,X>() {
 
             @Override
             public Optional<X> process(X x) {
                 return Optional.ofNullable(x);
             }
-
+            
         };
     }
 
     @Override
     public <X> Windowing<X, Pair<X, X>> pairing() {
-        return new Windowing<X, Pair<X, X>>() {
+        return new Windowing<X,Pair<X,X>>() {
 
             List<X> list = new ArrayList<>();
 
             @Override
             public Optional<Pair<X, X>> process(X x) {
-                if (list.isEmpty()) {
+                if (list.isEmpty()){
                     list.add(x);
                     return Optional.empty();
                 }
                 list.add(x);
                 return Optional.ofNullable(new Pair<>(list.get(list.size() - 2), list.get(list.size() - 1)));
             }
-
+            
         };
+    }
+
+    private int sum(List<Integer> list){
+        return list.stream().mapToInt(x -> x).sum();
     }
 
     @Override
     public Windowing<Integer, Integer> sumLastFour() {
-        return new Windowing<Integer, Integer>() {
+        return new Windowing<Integer,Integer>() {
             List<Integer> list = new ArrayList<>();
-
             @Override
             public Optional<Integer> process(Integer x) {
-                int somma = 0;
-                list.add(x);
-                if (list.size() < 4) {
+                if (list.size() < 3){
+                    list.add(x);
                     return Optional.empty();
                 }
-                for (int i = 1; i <= 4; i++) {
-                    somma = somma + list.get(list.size() - i);
-                }
-                return Optional.of(somma);
+                list.add(x);
+                return Optional.ofNullable(sum(list.subList(list.size() - 4, list.size())));
             }
-
         };
     }
 
     @Override
     public <X> Windowing<X, List<X>> lastN(int n) {
-        return new Windowing<X, List<X>>() {
+        return new Windowing<X,List<X>>() {
             List<X> list = new ArrayList<>();
-
             @Override
             public Optional<List<X>> process(X x) {
-                List<X> out = new ArrayList<>();
-
-                list.add(x);
-                if (list.size() < 4) {
+                if (list.size() < n - 1){
+                    list.add(x);
                     return Optional.empty();
                 }
-                for (int i = list.size() - 4; i <= list.size() - 1; i++) {
-                    out.add(list.get(i));
-                }
-                return Optional.ofNullable(out);
+                list.add(x);
+                return Optional.ofNullable(list.subList(list.size() - n , list.size()));
             }
-
+            
         };
     }
 

ac2740d5961324275e65e2b02d49fd782c6f1eba
Christian Kadiu - christian.kadiu@studio.unibo.it <christian.kadiu@studio.unibo.it>
2025-01-21 11:39:01 +0100

    minor changes

--- src/oop2023-esami/a02a/e2/Logics.java
+++ src/oop2023-esami/a02a/e2/Logics.java
@@ -2,8 +2,4 @@ package a02a.e2;
 
 public interface Logics {
 
-    boolean hit(Pair<Integer, Integer> pos);
-
-    boolean quit();
-
 }
--- src/oop2023-esami/a02a/e2/LogicsImpl.java
+++ src/oop2023-esami/a02a/e2/LogicsImpl.java
@@ -1,53 +1,5 @@
 package a02a.e2;
 
-import java.util.ArrayList;
-import java.util.List;
+public class LogicsImpl {
 
-public class LogicsImpl implements Logics {
-
-    int size;
-    List<Pair<Integer, Integer>>  lista;
-
-    LogicsImpl(int size){
-        this.size = size;
-        lista = new ArrayList<>();
-    }
-
-    @Override
-    public boolean hit(Pair<Integer, Integer> pos) {
-        if (isValid(pos)){
-            lista.add(pos);
-            print();
-            return true;
-        }
-        return false;
-    }
-
-    private boolean isValid(Pair<Integer, Integer> p){
-        if (p.getX() % 2 == 0 && p.getY() % 2 == 0){
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public boolean quit() {
-        if (lista.size() >= 4){
-            for (int i = 1; i < 4; i++){
-                int deltaX = Math.abs(lista.get(lista.size() - i).getX() - lista.get(lista.size() - (i + 1)).getX());
-                int deltaY = Math.abs(lista.get(lista.size() - i).getY() - lista.get(lista.size() - (i + 1)).getY());
-                if ((deltaX != 2 && deltaX != 0) || (deltaY != 2 && deltaY != 0)){
-                    return false;
-                }
-            }
-            return false;
-        }
-        return false;
-    }
-
-    private void print(){
-        for (Pair<Integer,Integer> pair : lista) {
-            System.out.println(pair);
-        }
-    }
 }

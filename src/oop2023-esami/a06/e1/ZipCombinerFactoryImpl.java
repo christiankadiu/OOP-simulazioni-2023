d7b2eb493f29b82495599420b904e0819bf7e4a1
christian <kadiuchristian8@gmail.com>
2025-02-03 20:02:00 +0100

    solved exercise e1 in a06

--- src/oop2023-esami/a06/e1/ZipCombinerFactoryImpl.java
+++ src/oop2023-esami/a06/e1/ZipCombinerFactoryImpl.java
@@ -1,90 +1,97 @@
 package a06.e1;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-public class ZipCombinerFactoryImpl implements ZipCombinerFactory{
+public class ZipCombinerFactoryImpl implements ZipCombinerFactory {
 
     @Override
     public <X, Y> ZipCombiner<X, Y, Pair<X, Y>> classical() {
-        return new ZipCombiner<X,Y,Pair<X,Y>>() {
-            List<Pair<X, Y>> lista = new ArrayList<>();
+        return new ZipCombiner<X, Y, Pair<X, Y>>() {
+
             @Override
             public List<Pair<X, Y>> zipCombine(List<X> l1, List<Y> l2) {
-                int current = 0;
-                for (X x : l1) {
-                    lista.add(new Pair<X,Y>(x, l2.get(current++)));
+                List<Pair<X, Y>> out = new ArrayList<>();
+                for (int i = 0; i < l1.size(); i++) {
+                    out.add(new Pair<X, Y>(l1.get(i), l2.get(i)));
                 }
-                return lista;
+                return out;
             }
-            
+
         };
     }
 
     @Override
     public <X, Y, Z> ZipCombiner<X, Y, Z> mapFilter(Predicate<X> predicate, Function<Y, Z> mapper) {
-        return new ZipCombiner<X,Y,Z>() {
-
-            List<Z> lista = new ArrayList<>();
-            Y y;
+        return new ZipCombiner<X, Y, Z>() {
 
             @Override
             public List<Z> zipCombine(List<X> l1, List<Y> l2) {
-                Iterator<Y> it = l2.iterator();
-                for (X x : l1) {
-                    if (it.hasNext()){
-                        y = it.next();
-                    }
-                    if (predicate.test(x)){
-                        lista.add(mapper.apply(y));
+                List<Z> out = new ArrayList<>();
+                for (int i = 0; i < l1.size(); i++) {
+                    if (predicate.test(l1.get(i))) {
+                        out.add(mapper.apply(l2.get(i)));
                     }
                 }
-                return lista;
-                
+                return out;
             }
-            
+
         };
     }
 
     @Override
     public <Y> ZipCombiner<Integer, Y, List<Y>> taker() {
-        return new ZipCombiner<Integer,Y,List<Y>>() {
-
-            List<List<Y>> out = new ArrayList<>();
+        return new ZipCombiner<Integer, Y, List<Y>>() {
 
             @Override
             public List<List<Y>> zipCombine(List<Integer> l1, List<Y> l2) {
-                for (Integer i : l1) {
-                    out.add(l2.subList(0, i));
+                List<List<Y>> out = new ArrayList<>();
+                for (int i = 0; i < l1.size(); i++) {
+                    out.add(get(l1.get(i), l2));
                 }
                 return out;
             }
-            
+
         };
+    }
+
+    private <X, Y> List<Y> get(int n, List<Y> l) {
+        return l.stream().limit(n).toList();
     }
 
     @Override
     public <X> ZipCombiner<X, Integer, Pair<X, Integer>> countUntilZero() {
-        return new ZipCombiner<X,Integer,Pair<X,Integer>>() {
-            List<Pair<X, Integer>> out = new ArrayList<>();
-            int count = 0;
-            int c;
+        return new ZipCombiner<X, Integer, Pair<X, Integer>>() {
+
             @Override
             public List<Pair<X, Integer>> zipCombine(List<X> l1, List<Integer> l2) {
-                for (X x : l1) {
-                    c = 0;
-                    while (l2.get(count++) != 0 && count < l2.size()){
-                        c++;
-                    }
-                    out.add(new Pair<X,Integer>(x, c));
+                List<Pair<X, Integer>> out = new ArrayList<>();
+                int prec = 0;
+                int c;
+                for (int i = 0; i < l1.size(); i++) {
+                    c = count(l2, prec);
+                    out.add(new Pair<X, Integer>(l1.get(i), c));
+                    prec = prec + c;
                 }
                 return out;
             }
-            
+
         };
     }
-    
+
+    private int count(List<Integer> l, int index) {
+        int c = 0;
+        for (int i = index; i < l.size(); i++) {
+            if (l.get(i) != 0) {
+                c++;
+            }
+            if (l.get(i) == 0 && i != index) {
+                break;
+            }
+        }
+        return c;
+    }
+
 }

b4144b036550cda898c074ce1725c6c212eef068
christian <kadiuchristian8@gmail.com>
2025-02-08 21:05:53 +0100

    minor changes

--- src/oop2023-esami/a02a/e1/ListBuilderFactoryImpl.java
+++ src/oop2023-esami/a02a/e1/ListBuilderFactoryImpl.java
@@ -4,74 +4,81 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class ListBuilderFactoryImpl implements ListBuilderFactory{
+public class ListBuilderFactoryImpl implements ListBuilderFactory {
+
+    public class ListBuilderImpl<T> implements ListBuilder<T> {
+
+        List<T> list = new ArrayList<>();
+
+        ListBuilderImpl(List<T> l) {
+            list = new ArrayList<>(l);
+        }
+
+        @Override
+        public ListBuilder<T> add(List<T> list) {
+            return new ListBuilderImpl<>(Stream.concat(this.list.stream(), list.stream()).toList());
+        }
+
+        @Override
+        public ListBuilder<T> concat(ListBuilder<T> lb) {
+            return add(lb.build());
+        }
+
+        @Override
+        public ListBuilder<T> replaceAll(T t, ListBuilder<T> lb) {
+            boolean p = true;
+            while (p) {
+                int index = list.indexOf(t);
+                if (index == -1) {
+                    p = false;
+                }
+                replace(list, index, lb.build());
+            }
+            return this;
+        }
+
+        @Override
+        public ListBuilder<T> reverse() {
+            Collections.reverse(list);
+            return this;
+        }
+
+        @Override
+        public List<T> build() {
+            return list;
+        }
+
+    }
+
+    private <T> List<T> replace(List<T> input, int i, List<T> l) {
+        input.addAll(i, l);
+        if (i + l.size() < input.size()) {
+            l.remove(i + l.size());
+        }
+        return l;
+    }
 
     @Override
     public <T> ListBuilder<T> empty() {
-        return (ListBuilder<T>) fromList(Stream.of().toList());
+        return new ListBuilderImpl<T>((List<T>) Stream.of().toList());
     }
 
     @Override
     public <T> ListBuilder<T> fromElement(T t) {
-        return fromList(Stream.of(t).toList());
+        return new ListBuilderImpl<>(Stream.of(t).toList());
     }
 
+    @Override
     public <T> ListBuilder<T> fromList(List<T> list) {
-        return new ListBuilder<T>() {
-
-            List<T> l = new ArrayList<>(list);
-
-            @Override
-            public ListBuilder<T> add(List<T> list) {
-                return fromList(Stream.concat(l.stream(), list.stream()).toList());
-            }
-
-            @Override
-            public ListBuilder<T> concat(ListBuilder<T> lb) {
-                return add(lb.build());
-            }
-
-            @Override
-            public ListBuilder<T> replaceAll(T t, ListBuilder<T> lb) {
-                for (int i = 0; i < l.size(); i++){
-                    if (l.get(i).equals(t)){
-                        l = modify(l, lb.build(), i);
-                    }
-                }
-                return fromList(l);
-            }
-
-            private List<T> modify(List<T> input, List<T> target, int index){
-                input.addAll(index, target);
-                input.remove(index + target.size());
-                return input;
-            }
-
-            @Override
-            public ListBuilder<T> reverse() {
-                var l2 = new ArrayList<>(l);
-                Collections.reverse(l2);
-                return fromList(l2);
-            }
-
-            @Override
-            public List<T> build() {
-                return this.l;
-            }
-            
-        };
+        return new ListBuilderImpl<>(list);
     }
-    
+
     @Override
     public <T> ListBuilder<T> join(T start, T stop, List<ListBuilder<T>> builderList) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'join'");
     }
 
-    
-    
 }
--- src/oop2023-esami/a02a/e2/GUI.java
+++ src/oop2023-esami/a02a/e2/GUI.java
@@ -7,46 +7,32 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
-    
+
     private static final long serialVersionUID = -6218820567019985015L;
-    private final Map<JButton, Pair<Integer, Integer>> cells = new HashMap<>();
-    Logics logic;
-    
+    private final List<JButton> cells = new ArrayList<>();
+
     public GUI(int size) {
-        logic = new LogicsImpl(size);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(100*size, 100*size);
-        
-        JPanel panel = new JPanel(new GridLayout(size,size));
+        this.setSize(100 * size, 100 * size);
+
+        JPanel panel = new JPanel(new GridLayout(size, size));
         this.getContentPane().add(panel);
-        
+
         ActionListener al = e -> {
-            var jb = (JButton)e.getSource();
-        	Pair<Integer, Integer> pos = cells.get(jb);
-            if (logic.hit(pos)){
-                jb.setText("O");
-            }
-            if (logic.quit()){
-                System.exit(0);
-            }
+            var jb = (JButton) e.getSource();
+            jb.setText(String.valueOf(cells.indexOf(jb)));
         };
-                
-        for (int i=0; i<size; i++){
-            for (int j=0; j<size; j++){
-            	var pos = new Pair<>(j,i);
-                final JButton jb = new JButton();
-                if (i % 2 == 0 && j % 2 != 0){
-                    jb.setText("*");
-                }
-                if (i % 2 != 0){
-                    jb.setText("*");
-                }
-                this.cells.put(jb, pos);
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                var pos = new Pair<>(j, i);
+                final JButton jb = new JButton(pos.toString());
+                this.cells.add(jb);
                 jb.addActionListener(al);
                 panel.add(jb);
             }
         }
         this.setVisible(true);
     }
-    
+
 }
